68437a25366f243e520eec1e7538b6b6f72c3259
Faris Adlin <faris.adlin@xapiens.id>
2021-04-01 10:57:04 +0700

    combine reducers

---
+++ src/redux/reducers/counterReducer.js
@@ -0,0 +1,30 @@
+import {DECREMENT_COUNTER, INCREMENT_COUNTER, RESET_COUNTER} from '../actions/actionTypes'
+
+const initialState = {
+    count: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+    const {type, payload} = action
+    switch(type) {
+        case INCREMENT_COUNTER:
+            return{
+                ...state, 
+                count: state.count + payload
+            }
+        case DECREMENT_COUNTER:
+            return {
+                ...state,
+                count: state.count - payload
+            }
+        case RESET_COUNTER:
+            return {
+                ...state,
+                count: 0
+            }
+        default:
+            return state
+    }
+}
+
+export default counterReducer;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,30 +1,6 @@
-import {DECREMENT_COUNTER, INCREMENT_COUNTER, RESET_COUNTER} from '../actions/actionTypes'
+import {combineReducers as combiner} from 'redux'
+import counterReducer from './counterReducer'
 
-const initialState = {
-    count: 0
-}
-
-const rootReducer = (state = initialState, action) => {
-    const {type, payload} = action
-    switch(type) {
-        case INCREMENT_COUNTER:
-            return{
-                ...state, 
-                count: state.count + payload
-            }
-        case DECREMENT_COUNTER:
-            return {
-                ...state,
-                count: state.count - payload
-            }
-        case RESET_COUNTER:
-            return {
-                ...state,
-                count: 0
-            }
-        default:
-            return state
-    }
-}
-
-export default rootReducer;
+export default combiner({
+    counter: counterReducer
+})
